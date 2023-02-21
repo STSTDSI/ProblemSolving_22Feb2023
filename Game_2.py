e3770e3e138559a83eb49939e49b2d6f259e6a0d
She and They do Science and Tech (STST) <125907257+STSTDSI@users.noreply.github.com>
2023-02-21 13:34:59 +0000

    Create Game_2.py

---
+++ Game_2.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+import os
+from pygame.locals import *
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((600, 480))
+bg = pygame.image.load(os.path.join("./", "back.png"))
+pygame.mouse.set_visible(0)
+pygame.display.set_caption('Garden Game')
+
+while True:
+
+    clock.tick(60)
+
+    screen.blit(bg, (0, 0))
+
+    x, y = pygame.mouse.get_pos()
+
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+
+            sys.exit()
+    pygame.display.update()
