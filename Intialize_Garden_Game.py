4d0bb25d4b41ea2561005d2af0f7878ee892286d
She and They do Science and Tech (STST) <125907257+STSTDSI@users.noreply.github.com>
2023-02-21 14:53:05 +0000

    Update and rename Game_2.py to Intialize_Garden_Game.py

--- Intialize_Garden_Game.py
+++ Intialize_Garden_Game.py
@@ -2,16 +2,17 @@ import pygame
 import sys
 import os
 from pygame.locals import *
+from pygame.locals import ( K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, QUIT,)
 
 pygame.init()
 clock = pygame.time.Clock()
+# Define constants for the screen width and height
 screen = pygame.display.set_mode((600, 480))
 bg = pygame.image.load(os.path.join("./", "back.png"))
 pygame.mouse.set_visible(0)
 pygame.display.set_caption('Garden Game')
 
 while True:
-
     clock.tick(60)
 
     screen.blit(bg, (0, 0))
